a1a03f3c0be7c00a42bfaff0fb14969bec6e93ca
Boris Ding <ding@pleisto.com>
2025-04-15 00:04:37 +0800

    chore: update version to 1.5.5 and modify tsup configuration for Node 22 compatibility

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  target: ['node20'],
-  shims: true,
-  minify: true,
-  treeshake: true
+  target: ['node22'],
+  shims: false,
+  minify: false,
+  treeshake: false,
+  skipNodeModulesBundle: true,
+  bundle: false
 })
